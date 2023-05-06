8e43540ebdead26378c2b6d044c173699b2fd20e
PavelM <myagkiypavel@gmail.com>
2023-05-06 12:21:04 +0300

    feature: init tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        sm: "321px",
+        md: "480px",
+        lg: "768px",
+        xl: "1920px",
+      },
+      padding: {
+        DEFAULT: "20px",
+        // sm: '30px',
+        // md: '50px',
+      },
+    },
+    extend: {
+      screens: {
+        sm: "321px",
+        md: "481px",
+        lg: "769px",
+        xl: "1920px",
+      },
+    },
   },
   plugins: [],
-}
+};
