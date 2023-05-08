1818e0a7b3f1658e1e8cd03e2e9e13bf2c146b37
PavelM <myagkiypavel@gmail.com>
2023-05-08 12:50:30 +0300

    chore: mainCard

--- src/components/MainCard/MainCard.tsx
+++ src/components/MainCard/MainCard.tsx
@@ -1,20 +1,21 @@
 import { FC } from "react";
+import classNames from "classnames";
 
 import styles from "./mainCard.module.scss";
+import { Card } from "../../types";
 
-type Props = {
-  src: string;
-  alt: string;
-  title: string;
-  text: string;
-};
+type Props = Omit<Card, "id"> & { typeMd?: boolean };
 
-export const MainCard: FC<Props> = ({ title, text, src, alt }) => {
+export const MainCard: FC<Props> = ({ title, text, src, alt, typeMd }) => {
   return (
     <div className={styles.card}>
-      <img src={src} alt={alt} />
-      <h3>{title}</h3>
-      <p>{text}</p>
+      <img
+        className={classNames({ [styles.typeMd]: typeMd })}
+        src={src}
+        alt={alt}
+      />
+      <h3 className={classNames({ [styles.typeMd]: typeMd })}>{title}</h3>
+      <p className={classNames({ [styles.typeMd]: typeMd })}>{text}</p>
     </div>
   );
 };
