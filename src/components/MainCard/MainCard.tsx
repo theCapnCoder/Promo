70601c84d4f6312b8241aa78f2fff02ae9debe8e
PavelM <myagkiypavel@gmail.com>
2023-05-09 13:01:26 +0300

    fix: MainCard

--- src/components/MainCard/MainCard.tsx
+++ src/components/MainCard/MainCard.tsx
@@ -4,18 +4,30 @@ import classNames from "classnames";
 import styles from "./mainCard.module.scss";
 import { Card } from "../../types";
 
-type Props = Omit<Card, "id"> & { typeMd?: boolean };
+type Props = Omit<Card, "id"> & {
+  typeMd?: boolean;
+  type: "offer" | "free" | "main";
+};
 
-export const MainCard: FC<Props> = ({ title, text, src, alt, typeMd }) => {
+export const MainCard: FC<Props> = ({
+  title,
+  text,
+  src,
+  alt,
+  typeMd,
+  type,
+}) => {
   return (
-    <div className={styles.card}>
+    <div className={classNames(styles.card, styles[type])}>
       <img
         className={classNames({ [styles.typeMd]: typeMd })}
         src={src}
         alt={alt}
       />
       <h3 className={classNames({ [styles.typeMd]: typeMd })}>{title}</h3>
-      <p className={classNames({ [styles.typeMd]: typeMd })}>{text}</p>
+      <p className={classNames({ [styles.typeMd]: typeMd }, styles[type])}>
+        {text}
+      </p>
     </div>
   );
 };
