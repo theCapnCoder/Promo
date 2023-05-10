0bb3ba06cba8fd2c2db2bc1422e64332c993c8db
PavelM <myagkiypavel@gmail.com>
2023-05-10 17:42:55 +0300

    style: Discount lg

--- src/blocks/Discount/Discount.tsx
+++ src/blocks/Discount/Discount.tsx
@@ -10,17 +10,20 @@ export const Discount = () => {
       <div className={styles.imgWrapper}>
         <img src={discount} alt="discount" />
       </div>
-      <h2>
-        <span>Получите </span>
-        <span className={styles.extraText}>крутую</span>
-        <span> скидку </span>
-        до 50%
-      </h2>
-      <h3>
-        Оставьте заявку сейчас и получите персональную скидку на первый месяц
-        аренды или бесплатный переезд
-      </h3>
-      <Button large text="Оставить заявку" />
+
+      <div className={styles.textBlock}>
+        <h2>
+          <span>Получите </span>
+          <span className={styles.extraText}>крутую</span>
+          <span> скидку </span>
+          до 50%
+        </h2>
+        <h3>
+          Оставьте заявку сейчас и получите персональную скидку на первый месяц
+          аренды или бесплатный переезд
+        </h3>
+        <Button large text="Оставить заявку" />
+      </div>
     </div>
   );
 };
